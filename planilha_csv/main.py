73f5d390e146fd0222ee6d5971b88e4c3e580548
martins9 <saulomartins.martins@gmail.com>
2017-12-08 16:56:11 -0200

    Novos commits

--- planilha_csv/main.py
+++ planilha_csv/main.py
@@ -1,5 +1,5 @@
-import csv
-from t_limpeza.t_function_limpeza import tirar_pontos_virgulas,deletar
+#import csv
+from t_limpeza.t_function_limpeza import *
 
 # Insercao de informacoes
 n_demanda = raw_input("Insira o numero da demanda:")
@@ -7,40 +7,18 @@ arquivo = raw_input("Insira o nome do arquivo:")
 n_issue = raw_input("Insira numero do Test Set:")
 n_assigne = raw_input("Insire Assigne:")
 
-# Tirando as virgulas do arquivo
-tirar_pontos_virgulas(arquivo, n_demanda)
+# Substituir virgula por ponto
+#substituir_pontos(n_demanda,arquivo)
+
+# Substituir virgulas do arquivo por ponto
+#substituir_ponto_virgula(n_demanda)
+
+# Tirando tracos do arquivo
+#tirar_tracos(n_demanda)
 
 arq2 = open(n_demanda + '_final.csv', 'w')
-with open(n_demanda + "_1.csv", 'rU') as csvIN:
-    outCSV = (line for line in csv.reader(csvIN, dialect='excel'))
+with open(arquivo, 'rU') as csvIN:
+    outCSV = (line for line in csv.reader(csvIN, delimiter=';', dialect='excel'))
 
-    part0 = "Custom field(Test Sets association with a Test);Summary;Assignee;Reporter;Issue Type;Description;" + \
-          "Test Type;Manual Test Steps"+"\n"
-    arq2.writelines(part0)
-
-    count = 0
     for row in outCSV:
-        count += 1
-        if count > 1:
-            a = row[1].split('\n')
-            count1 = 0
-
-            part1=n_issue + ";" + row[0] + ";" + n_assigne + ";;" + "Test;;Manual;"+'"[',
-            arq2.writelines(part1)
-
-            for teste in range(len(a)):
-                if teste != (len(a) - 1):
-                    part2 = '{ ""index"": ' + str(count1) + ', ' + '""step"": ""' + a[teste] + '"", ""data"": """", ""result"": """" }, ',
-                    arq2.writelines(part2)
-                else:
-                    part3 = '{ ""index"": ' + str(count1) + ', ' + '""step"": ""' + a[teste] + '"", ""data"": """", ""result"": ""' + \
-                            row[2].replace("\n",". ") + '"" } ]"',
-                    arq2.writelines(part3)
-                    arq2.writelines("\n")
-                count1 += 1
-
-# Deletar arquivo
-deletar(n_demanda+"_1.csv")
-
-csvIN.close()
-arq2.close()
+        print row[0]
--- t_limpeza/t_function_limpeza.py
+++ t_limpeza/t_function_limpeza.py
@@ -1,9 +1,61 @@
-import sys,os
+import sys,os,csv,re
 
-def tirar_pontos_virgulas(a,b):
+def deletar(c):
+    os.remove(c)
+
+def tirar_tracos(d):
     try:
-        arq1=open(a,'r')
-        arq2=open(b+'_1.csv','w')
+        arq3 = open(d + '_1.csv', 'w')
+        with open(d + '_2.csv', 'rU') as f:
+            text = (line for line in csv.reader(f,delimiter='|',dialect='excel'))
+            for row in text:
+                #row_new = row[2].split("\n")
+                #row_new_1 =row[3].split("\n")
+
+                print row[0]
+
+                #arq3.writelines(row_new_2)
+
+        arq3.close()
+        f.close()
+    except OSError:
+        print "Arquivo vazio"
+        sys.exit()
+
+def substituir_pontos(e,f):
+    arq4 = open(e + '_1.csv', 'w')
+    with open(f, 'rU') as f:
+        outCSV = (line for line in csv.reader(f, delimiter=';', dialect='excel'))
+
+        part0 = "Custom field(Test Sets association with a Test);Summary;Assignee;Reporter;Issue Type;Description;" + \
+                "Test Type;Manual Test Steps" + "\n"
+        print part0
+
+        count=0
+        for row in outCSV:
+            count += 1
+            if count > 1:
+
+                part1 = n_issue + ";" + row[0] + ";" + n_assigne + ";;" + "Test;;Manual;" + '"[',
+                a = row[0].split('\n')
+                for i in a:
+                    b = re.sub('(?<=\d),(?=\d)', '.', i)
+                    final1=b+','+row[1]+','
+                    arq4.writelines(final1)
+
+                b = row[2].split('\n')
+                for t in b:
+                    row[2].replace("-","")
+
+
+
+    arq4.close()
+    f.close()
+
+def substituir_ponto_virgula(b):
+    try:
+        arq1=open(b + '_1.csv','r')
+        arq2=open(b + '_2.csv','w')
 
         for line in arq1:
             new_lines=line.replace(";",",")
@@ -14,6 +66,3 @@ def tirar_pontos_virgulas(a,b):
     except OSError:
         print "Arquivo vazio"
         sys.exit()
-
-def deletar(c):
-    os.remove(c)
